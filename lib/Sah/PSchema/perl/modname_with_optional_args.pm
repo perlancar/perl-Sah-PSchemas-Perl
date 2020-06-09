bcc56ee7aa682fca5d1e3c6c20f05ed06499c397
perlancar (on pc-home) <perlancar@gmail.com>
2020-06-09 17:38:42 +0700

    Add parameterized schema: perl::modname_with_optional_args

---
+++ lib/Sah/PSchema/perl/modname_with_optional_args.pm
@@ -0,0 +1,38 @@
+package Sah::PSchema::perl::modname_with_optional_args;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+sub meta {
+    my $class = shift;
+
+    return +{
+        v => 1,
+        args => {
+            ns_prefix => {
+                schema => 'perl::modname*',
+                default => '',
+            },
+        },
+    };
+}
+
+sub get_schema {
+    return ["perl::modname_with_optional_prefix" => {
+        'x.perl.coerce_rules' => [
+            ['From_str::normalize_perl_modname' => {ns_prefix=>$args->{ns_prefix}}],
+        ],
+
+        'x.completion' => ['perl_modname' => {ns_prefix=>$args->{ns_prefix}}],
+        %{ $merge || {} },
+    }, {}];
+}
+
+1;
+# ABSTRACT: Perl module name with optional args (parameterized)
+
+=head1 SEE ALSO
+
+L<Sah::Schema::perl::modname_with_optional_args>
